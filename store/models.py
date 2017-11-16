224328c6f1c74f827808c067ae5f59d6fc7d2c11
hunter <darthicer@gmail.com>
2017-11-15 22:04:40 -0500

    fixed some bs and added book model book admin and changed store template

--- store/models.py
+++ store/models.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.utils import timezone
+
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    pub_date = models.DateField(default=timezone.now)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+    stock = models.IntegerField(default=0)
